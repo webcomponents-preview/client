4f84d3631c744eaf11f829770e29d2073ef3bf02
David Enke <david@enke.dev>
2023-03-15 03:39:36 +0100

    feat: set explicit color scheme

--- src/components/feature/toggle-color-scheme/toggle-color-scheme.component.ts
+++ src/components/feature/toggle-color-scheme/toggle-color-scheme.component.ts
@@ -1,6 +1,8 @@
 import { LitElement, type TemplateResult, html, unsafeCSS } from 'lit';
 import { customElement, eventOptions } from 'lit/decorators.js';
 
+import { ColorSchemable } from '@/utils/color-scheme.utils';
+
 import styles from './toggle-color-scheme.component.scss';
 
 /**
@@ -12,11 +14,17 @@ import styles from './toggle-color-scheme.component.scss';
  * ```
  */
 @customElement('wcp-toggle-color-scheme')
-export class ToggleColorScheme extends LitElement {
+export class ToggleColorScheme extends ColorSchemable(LitElement) {
   static readonly styles = unsafeCSS(styles);
 
+  override colorScheme = matchMedia('(prefers-color-scheme: dark)').matches ? ('dark' as const) : ('light' as const);
+
   @eventOptions({ passive: true })
-  handleButtonClick() {}
+  handleButtonClick() {
+    const detail = this.colorScheme === 'dark' ? 'light' : 'dark';
+    const event = new CustomEvent('wcp-color-scheme:toggle', { detail });
+    window.dispatchEvent(event);
+  }
 
   protected render(): TemplateResult {
     return html`
--- src/utils/color-scheme.utils.ts
+++ src/utils/color-scheme.utils.ts
@@ -4,6 +4,12 @@ import { property } from 'lit/decorators/property.js';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Constructor<T> = new (...args: any[]) => T;
 
+declare global {
+  interface WindowEventMap {
+    'wcp-color-scheme:toggle': CustomEvent<'dark' | 'light' | null>;
+  }
+}
+
 export declare class ColorSchemableInterface {
   colorScheme?: 'light' | 'dark';
 }
@@ -12,6 +18,20 @@ export const ColorSchemable = <T extends Constructor<LitElement>>(superClass: T)
   class ColorSchemableElement extends superClass {
     @property({ type: String, reflect: true, attribute: 'color-scheme' })
     colorScheme?: 'light' | 'dark';
+
+    handleColorSchemeToggle = (({ detail }: CustomEvent<'dark' | 'light' | null>) => {
+      this.colorScheme = detail ?? undefined;
+    }).bind(this);
+
+    override connectedCallback() {
+      super.connectedCallback();
+      window.addEventListener('wcp-color-scheme:toggle', this.handleColorSchemeToggle, false);
+    }
+
+    override disconnectedCallback() {
+      super.disconnectedCallback();
+      window.removeEventListener('wcp-color-scheme:toggle', this.handleColorSchemeToggle, false);
+    }
   }
   return ColorSchemableElement as Constructor<ColorSchemableInterface> & T;
 };
