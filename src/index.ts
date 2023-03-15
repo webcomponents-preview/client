1717eb4c2e42b2909d4f74d3903f2d660dd91185
David Enke <david@enke.dev>
2023-03-15 01:19:16 +0100

    chore: separated toggle button
    for sidebar interaction

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export * from './components/feature/preview-controls/preview-controls.component'
 export * from './components/feature/preview-details/preview-details.component';
 export * from './components/feature/preview-frame/preview-frame.component';
 export * from './components/feature/readme/readme.component';
+export * from './components/feature/toggle-sidebar/toggle-sidebar.component';
 export * from './components/layout/aside/aside.component';
 export * from './components/layout/layout/layout.component';
 export * from './components/layout/main/main.component';
