beb324ddcc0a80d8ad106c1f00247d741f522095
David Enke <david@enke.dev>
2023-03-15 01:22:46 +0100

    feat: prepared toggle button

---
+++ src/components/feature/toggle-color-scheme/README.md
@@ -0,0 +1,15 @@
+# wcp-toggle-color-scheme
+
+Shows a button to toggle the desired color-scheme.
+
+## Example
+
+```html
+<wcp-wcp-toggle-color-scheme></wcp-wcp-toggle-color-scheme>
+```
+
+## Methods
+
+| Method              | Type       |
+|---------------------|------------|
+| `handleButtonClick` | `(): void` |
---
+++ src/components/feature/toggle-color-scheme/toggle-color-scheme.component.ts
@@ -0,0 +1,32 @@
+import { LitElement, type TemplateResult, html, unsafeCSS } from 'lit';
+import { customElement, eventOptions } from 'lit/decorators.js';
+
+import styles from './toggle-color-scheme.component.scss';
+
+/**
+ * Shows a button to toggle the desired color-scheme.
+ *
+ * @example
+ * ```html
+ * <wcp-wcp-toggle-color-scheme></wcp-wcp-toggle-color-scheme>
+ * ```
+ */
+@customElement('wcp-toggle-color-scheme')
+export class ToggleColorScheme extends LitElement {
+  static readonly styles = unsafeCSS(styles);
+
+  @eventOptions({ passive: true })
+  handleButtonClick() {}
+
+  protected render(): TemplateResult {
+    return html`
+      <wcp-button kind="icon" @click="${this.handleButtonClick}"><wcp-icon name="menu"></wcp-icon></wcp-button>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'wcp-toggle-color-scheme': ToggleColorScheme;
+  }
+}
