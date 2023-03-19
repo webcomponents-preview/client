d7e2a68f9a997018f427f27e821e4eb71519fc2b
autoreleaser <release@enke.dev>
2023-03-19 23:31:08 +0000

    872b3cea88baab99ee155846eafe06c54aeab7d0

--- pr/18/components/feature/viewer/viewer.utils.d.ts
+++ pr/18/components/feature/viewer/viewer.utils.d.ts
@@ -12,7 +12,7 @@ export declare const EMPTY_ELEMENT_DATA: {
  */
 export declare function litKey(field: CustomElementField): string;
 export declare function prepareInitialElementData(element: CustomElementDeclaration): CustomElementData;
-export declare function parseMemberValue(input: Element | RadioNodeList, value: FormDataEntryValue): string | number | boolean | undefined;
+export declare function parseMemberValue(value: FormDataEntryValue, input: Element | RadioNodeList): FormDataEntryValue | string | number | boolean | undefined;
 export declare function mapMemberData(value: FormDataEntryValue, input: Element | RadioNodeList, field: CustomElementField): [string, unknown];
 export declare function mapSlotData(name: string, value: string): [string, string];
 export declare function mapFormData(form: HTMLFormElement, element: CustomElementDeclaration): CustomElementData;
