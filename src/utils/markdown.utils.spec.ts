602bd7c816d6780280a8f706bc20512d637c93bf
David Enke <david@enke.dev>
2023-05-09 19:43:51 +0200

    fixed relative urls with paths
    as we just missed to pass the path

--- src/utils/markdown.utils.spec.ts
+++ src/utils/markdown.utils.spec.ts
@@ -13,30 +13,41 @@ describe('markdown.utils', () => {
     });
 
     it('prefixes relative markdown urls with hashes', () => {
-      expect(prefixRelativeUrls('[foo](bar.md#hash)', 'baz/foo.md')).toBe('[foo](bar.md/hash)');
-      expect(prefixRelativeUrls('[foo](./bar.md#hash)', 'baz/foo.md')).toBe('[foo](bar.md/hash)');
-      expect(prefixRelativeUrls('[foo](bar.mdx#hash)', 'baz/foo.md')).toBe('[foo](bar.mdx/hash)');
-      expect(prefixRelativeUrls('[foo](./bar.mdx#hash)', 'baz/foo.md')).toBe('[foo](bar.mdx/hash)');
+      expect(prefixRelativeUrls('[foo](bar.md#hash)', 'foo.md')).toBe('[foo](bar.md/hash)');
+      expect(prefixRelativeUrls('[foo](./bar.md#hash)', 'foo.md')).toBe('[foo](bar.md/hash)');
+      expect(prefixRelativeUrls('[foo](bar.md#hash)', 'baz/foo.md')).toBe('[foo](baz%2Fbar.md/hash)');
+      expect(prefixRelativeUrls('[foo](./bar.md#hash)', 'baz/foo.md')).toBe('[foo](baz%2Fbar.md/hash)');
+
+      expect(prefixRelativeUrls('[foo](bar.mdx#hash)', 'foo.md')).toBe('[foo](bar.mdx/hash)');
+      expect(prefixRelativeUrls('[foo](./bar.mdx#hash)', 'foo.md')).toBe('[foo](bar.mdx/hash)');
+      expect(prefixRelativeUrls('[foo](bar.mdx#hash)', 'baz/foo.md')).toBe('[foo](baz%2Fbar.mdx/hash)');
+      expect(prefixRelativeUrls('[foo](./bar.mdx#hash)', 'baz/foo.md')).toBe('[foo](baz%2Fbar.mdx/hash)');
     });
 
     it('prefixes relative markdown urls with subpath and hashes', () => {
       expect(prefixRelativeUrls('[foo](baz/bar.md#hash)', 'foo.md')).toBe('[foo](baz%2Fbar.md/hash)');
       expect(prefixRelativeUrls('[foo](./baz/bar.md#hash)', 'foo.md')).toBe('[foo](baz%2Fbar.md/hash)');
+      expect(prefixRelativeUrls('[foo](baz/bar.md#hash)', 'foo/foo.md')).toBe('[foo](foo%2Fbaz%2Fbar.md/hash)');
+      expect(prefixRelativeUrls('[foo](./baz/bar.md#hash)', 'foo/foo.md')).toBe('[foo](foo%2Fbaz%2Fbar.md/hash)');
+      
       expect(prefixRelativeUrls('[foo](baz/bar.mdx#hash)', 'foo.md')).toBe('[foo](baz%2Fbar.mdx/hash)');
       expect(prefixRelativeUrls('[foo](./baz/bar.mdx#hash)', 'foo.md')).toBe('[foo](baz%2Fbar.mdx/hash)');
+      expect(prefixRelativeUrls('[foo](baz/bar.mdx#hash)', 'foo/foo.md')).toBe('[foo](foo%2Fbaz%2Fbar.mdx/hash)');
+      expect(prefixRelativeUrls('[foo](./baz/bar.mdx#hash)', 'foo/foo.md')).toBe('[foo](foo%2Fbaz%2Fbar.mdx/hash)');
     });
 
     it('prefixes relative markdown files with additional route', () => {
-      expect(prefixRelativeUrls('[foo](bar.md)', 'baz/foo.md', '/#/readme/')).toBe('[foo](/#/readme/bar.md)');
+      expect(prefixRelativeUrls('[foo](bar.md)', 'baz/foo.md', '/#/readme/')).toBe('[foo](/#/readme/baz%2Fbar.md)');
       expect(prefixRelativeUrls('[foo](baz/bar.md)', 'foo.md', '/#/readme/')).toBe('[foo](/#/readme/baz%2Fbar.md)');
-      expect(prefixRelativeUrls('[foo](bar.mdx)', 'baz/foo.md', '/#/readme/')).toBe('[foo](/#/readme/bar.mdx)');
+
+      expect(prefixRelativeUrls('[foo](bar.mdx)', 'baz/foo.md', '/#/readme/')).toBe('[foo](/#/readme/baz%2Fbar.mdx)');
       expect(prefixRelativeUrls('[foo](baz/bar.mdx)', 'foo.md', '/#/readme/')).toBe('[foo](/#/readme/baz%2Fbar.mdx)');
     });
 
     it('prefixes relative markdown files with additional route and hashes', () => {
-      expect(prefixRelativeUrls('[foo](bar.md#hash)', 'baz/foo.md', '/#/')).toBe('[foo](/#/bar.md/hash)');
+      expect(prefixRelativeUrls('[foo](bar.md#hash)', 'baz/foo.md', '/#/')).toBe('[foo](/#/baz%2Fbar.md/hash)');
       expect(prefixRelativeUrls('[foo](baz/bar.md#hash)', 'foo.md', '/#/')).toBe('[foo](/#/baz%2Fbar.md/hash)');
-      expect(prefixRelativeUrls('[foo](bar.mdx#hash)', 'baz/foo.md', '/#/')).toBe('[foo](/#/bar.mdx/hash)');
+      expect(prefixRelativeUrls('[foo](bar.mdx#hash)', 'baz/foo.md', '/#/')).toBe('[foo](/#/baz%2Fbar.mdx/hash)');
       expect(prefixRelativeUrls('[foo](baz/bar.mdx#hash)', 'foo.md', '/#/')).toBe('[foo](/#/baz%2Fbar.mdx/hash)');
     });
 
