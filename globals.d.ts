d0e07bf4b63421cb744dd9cac0acd897a860fa34
David Enke <david@enke.dev>
2023-05-10 09:06:05 +0200

    use now available types
    of esbuild-copy-static-files plugin provided by DefinitlyTyped

--- globals.d.ts
+++ globals.d.ts
@@ -1,20 +1,3 @@
-declare module 'esbuild-copy-static-files' {
-  import type { Plugin } from 'esbuild';
-  export type CopyStaticFilesOptions = {
-    src: string;
-    dest: string;
-    filter: (src: string, dest: string) => boolean;
-
-    dereference: boolean;
-    errorOnExist: boolean;
-    force: boolean;
-    preserveTimestamps: boolean;
-    recursive: boolean;
-  };
-  const copyStaticFiles: (options?: Partial<CopyStaticFilesOptions>) => Plugin;
-  export default copyStaticFiles;
-}
-
 // Tell Typescript that SASS imports are okay as they're handled by bundler
 declare module '*.scss' {
   export const styles: string;
