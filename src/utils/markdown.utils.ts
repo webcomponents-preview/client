c84414b281272f680a7f96529915a75cdc09b7a5
David Enke <david@enke.dev>
2023-05-10 10:01:36 +0200

    separated custom renderer declaration
    as we do not need to declare it anonymously every time we need an instance

--- src/utils/markdown.utils.ts
+++ src/utils/markdown.utils.ts
@@ -5,6 +5,27 @@ export function getCodeExample(slot: HTMLSlotElement): string {
   return slot.assignedElements().reduce((acc, el) => `${acc}\n${el.outerHTML}`, '');
 }
 
+export class Renderer extends marked.Renderer {
+  constructor(private readonly addCodePreview = true) {
+    super();
+  }
+
+  code(code: string, language = 'plaintext', escaped = false): string {
+    // do not use example component for anything but html examples
+    if (language !== 'html' || !this.addCodePreview) {
+      return `<wcp-code>${super.code(code, language, escaped)}</wcp-code>`;
+    }
+
+    // wrap the code in a custom element to preview it
+    return `
+      <wcp-markdown-example>
+        <wcp-code slot="code">${super.code(code, language, escaped)}</wcp-code>
+        <div slot="preview">${code}</div>
+      </wcp-markdown-example>
+    `;
+  }
+}
+
 /**
  * Only relative links will be handled. If a markdown file (*.md, *.mdx) is linked, it will be prefixed with the route additionally.
  */
@@ -30,25 +51,10 @@ export function prefixRelativeUrls(markdown: string, base: string, route = ''): 
 
 export function renderMarkdown(mardown: string, addCodePreview = true): string {
   return marked(mardown, {
-    highlight(code: string, lang: string) {
+    highlight(code, lang) {
       const language = hljs.getLanguage(lang) ? lang : 'plaintext';
       return hljs.highlight(code, { language }).value;
     },
-    renderer: new (class extends marked.Renderer {
-      code(code: string, language = 'plaintext', escaped = false): string {
-        // do not use example component for anything but html examples
-        if (language !== 'html' || !addCodePreview) {
-          return `<wcp-code>${super.code(code, language, escaped)}</wcp-code>`;
-        }
-
-        // wrap the code in a custom element to preview it
-        return `
-          <wcp-markdown-example>
-            <wcp-code slot="code">${super.code(code, language, escaped)}</wcp-code>
-            <div slot="preview">${code}</div>
-          </wcp-markdown-example>
-        `;
-      }
-    })(),
+    renderer: new Renderer(addCodePreview),
   });
 }
