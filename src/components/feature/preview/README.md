d532a858ca77fcbedf3a13ea2b1338fa421ecef5
David Enke <david@enke.dev>
2023-05-29 18:29:30 +0200

    chore(wcp-preview): introduce common preview component

---
+++ src/components/feature/preview/README.md
@@ -0,0 +1,15 @@
+# class: `Preview`
+
+## Methods
+
+| Name     | Privacy   | Description | Parameters | Return           | Inherited From |
+| -------- | --------- | ----------- | ---------- | ---------------- | -------------- |
+| `render` | protected |             |            | `TemplateResult` |                |
+
+## Slots
+
+| Name | Description             |
+| ---- | ----------------------- |
+|      | The content to preview. |
+
+<hr/>
---
+++ src/components/feature/preview/preview.component.ts
@@ -0,0 +1,33 @@
+import { LitElement, type TemplateResult, html, unsafeCSS } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import { ColorSchemable } from '@/utils/color-scheme.utils.js';
+
+import styles from './preview.component.scss';
+
+/**
+ * reviews given content.
+ *
+ * @example
+ * ```html
+ * <wcp-preview>
+ *   <wcp-button>Example button</wcp-button>
+ * </wcp-preview>
+ * ```
+ *
+ * @slot - The content to preview.
+ */
+@customElement('wcp-preview')
+export class Preview extends ColorSchemable(LitElement) {
+  static readonly styles = unsafeCSS(styles);
+
+  protected render(): TemplateResult {
+    return html`<slot></slot>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'wcp-preview': Preview;
+  }
+}
