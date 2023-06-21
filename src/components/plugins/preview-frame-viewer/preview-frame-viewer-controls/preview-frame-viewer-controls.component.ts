41da71602561d970e4362569f410ea975cc1abc7
David Enke <david@enke.dev>
2023-06-21 11:59:24 +0200

    feat(wcp-preview-frame-viewer-controls): render markdown in hints

--- src/components/plugins/preview-frame-viewer/preview-frame-viewer-controls/preview-frame-viewer-controls.component.ts
+++ src/components/plugins/preview-frame-viewer/preview-frame-viewer-controls/preview-frame-viewer-controls.component.ts
@@ -1,4 +1,5 @@
 import { html, LitElement, type TemplateResult, unsafeCSS, nothing } from 'lit';
+import { unsafeStatic, withStatic } from 'lit/static-html.js';
 import { customElement, property } from 'lit/decorators.js';
 
 import { map } from 'lit/directives/map.js';
@@ -6,6 +7,7 @@ import { when } from 'lit/directives/when.js';
 
 import type * as Parsed from '@/utils/parser.types.js';
 import { ColorSchemable } from '@/utils/color-scheme.utils.js';
+import { renderMarkdown } from '@/utils/markdown.utils.js';
 
 import type { ElementData } from '../preview-frame-viewer.utils.js';
 
@@ -13,6 +15,13 @@ import styles from './preview-frame-viewer-controls.component.scss';
 
 /**
  * @element wcp-preview-frame-viewer-controls
+ * 
+ * @cssprop --wcp-preview-frame-viewer-controls-headline-size - The font size of the headline.
+ * @cssprop --wcp-preview-frame-viewer-controls-headline-weight - The font weight of the headline.
+ * @cssprop --wcp-preview-frame-viewer-controls-headline-spacing - The inner spacing of the headline.
+ * 
+ * @cssprop --wcp-preview-frame-viewer-controls-dark-border-color - The border color of the element in dark mode.
+ * @cssprop --wcp-preview-frame-viewer-controls-light-border-color - The border color of the element in light mode.
  *
  * @emits {CustomEvent<FormData>} wcp-preview-frame-viewer-controls:input - Fires when the user changes a control value.
  */
@@ -25,6 +34,14 @@ export class PreviewFrameViewerControls extends ColorSchemable(LitElement) {
 
   @property({ type: Object })
   readonly data?: ElementData;
+
+  // content is derived from documentation which can be written in markdown
+  protected renderHint(content?: string): TemplateResult {
+    return when(
+      content,
+      () => withStatic(html)`<div slot="hint">${unsafeStatic(renderMarkdown(content as string, false))}</div>`
+    );
+  }
 
   protected renderFieldControl(field: Parsed.Field): TemplateResult {
     if (!field.isControllable) return html`${nothing}`;
@@ -39,7 +56,7 @@ export class PreviewFrameViewerControls extends ColorSchemable(LitElement) {
               label="${field.name}"
               ?checked="${Boolean(this.data?.fields[field.name])}"
             >
-              ${when(field.description, () => html`<div slot="hint">${field.description}</div>`)}
+              ${this.renderHint(field.description)}
             </wcp-input-checkbox>
           `
       )}
@@ -51,7 +68,7 @@ export class PreviewFrameViewerControls extends ColorSchemable(LitElement) {
             label="${field.attribute ?? field.name}"
             .value="${this.data?.fields[field.name] as string | undefined}"
           >
-            ${when(field.description, () => html`<div slot="hint">${field.description}</div>`)}
+            ${this.renderHint(field.description)}
           </wcp-input-text>
         `
       )}
@@ -63,7 +80,7 @@ export class PreviewFrameViewerControls extends ColorSchemable(LitElement) {
             label="${field.attribute ?? field.name}"
             .value="${this.data?.fields[field.name] as number | undefined}"
           >
-            ${when(field.description, () => html`<div slot="hint">${field.description}</div>`)}
+            ${this.renderHint(field.description)}
           </wcp-input-number>
         `
       )}
@@ -81,7 +98,7 @@ export class PreviewFrameViewerControls extends ColorSchemable(LitElement) {
                 <wcp-input-select-option label="${option}" .value="${option}"></wcp-input-select-option>
               `
             )}
-            ${when(field.description, () => html`<div slot="hint">${field.description}</div>`)}
+            ${this.renderHint(field.description)}
           </wcp-input-select>
         `
       )}
@@ -95,7 +112,7 @@ export class PreviewFrameViewerControls extends ColorSchemable(LitElement) {
         label="${slot.name.trim() ? slot.name : 'Default'}"
         .value="${this.data?.slots[slot.name]}"
       >
-        ${when(slot.hasDescription, () => html`<div slot="hint">${slot.description}</div>`)}
+        ${when(slot.hasDescription, () => html`${this.renderHint(slot.description)}`)}
       </wcp-input-text>
     `;
   }
