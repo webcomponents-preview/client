59ba439de09b247d047ad1cb66534fb59a78e007
David Enke <david@enke.dev>
2023-06-23 17:33:33 +0200

    docs(wcp-preview-hint): public API for CSS props

--- src/components/features/preview-hint/README.md
+++ src/components/features/preview-hint/README.md
@@ -17,3 +17,23 @@ Shows a hint to a given preview element.
 | Method           | Type       |
 |------------------|------------|
 | `updatePosition` | `(): void` |
+
+## CSS Custom Properties
+
+| Property                                       | Description                                      |
+|------------------------------------------------|--------------------------------------------------|
+| `--wcp-preview-hint-button-active-background`  | The background color of the hint button in active state. |
+| `--wcp-preview-hint-button-active-size`        | Size of the hint button in active state.         |
+| `--wcp-preview-hint-button-passive-background` | The background color of the hint button in passive state. |
+| `--wcp-preview-hint-button-passive-size`       | Size of the hint button in passive state.        |
+| `--wcp-preview-hint-debug-background-opacity`  | Opacity of the debugging fields background.      |
+| `--wcp-preview-hint-debug-border-width`        | Border width of the debugging fields.            |
+| `--wcp-preview-hint-debug-dark-background`     | Debugging field background color in dark mode.   |
+| `--wcp-preview-hint-debug-dark-stroke`         | Debugging field dash and border color in dark mode. |
+| `--wcp-preview-hint-debug-light-background`    | Debugging field background color in light mode.  |
+| `--wcp-preview-hint-debug-light-stroke`        | Debugging field dash and border color in light mode. |
+| `--wcp-preview-hint-debug-stripe-dash-gap`     | Gap between the dashes of the debugging field background. |
+| `--wcp-preview-hint-debug-stripe-dash-size`    | Length of the dashes of the debugging field background. |
+| `--wcp-preview-hint-debug-stripe-distance`     | Distance of the stripes of the debugging field background. |
+| `--wcp-preview-hint-debug-stripe-tilt`         | Tilt of the stripes of the debugging field background in degrees. |
+| `--wcp-preview-hint-debug-stripe-width`        | Width of the stripes of the debugging field background. |
--- src/components/features/preview-hint/preview-hint.component.ts
+++ src/components/features/preview-hint/preview-hint.component.ts
@@ -10,6 +10,26 @@ import styles from './preview-hint.component.scss';
  * Shows a hint to a given preview element.
  *
  * @element wcp-preview-hint
+ * 
+ * @cssprop --wcp-preview-hint-button-passive-background - The background color of the hint button in passive state.
+ * @cssprop --wcp-preview-hint-button-active-background - The background color of the hint button in active state.
+ * @cssprop --wcp-preview-hint-button-passive-size - Size of the hint button in passive state.
+ * @cssprop --wcp-preview-hint-button-active-size - Size of the hint button in active state.
+ *
+ * @cssprop --wcp-preview-hint-debug-border-width - Border width of the debugging fields.
+ * @cssprop --wcp-preview-hint-debug-background-opacity - Opacity of the debugging fields background.
+ *
+ * @cssprop --wcp-preview-hint-debug-stripe-distance - Distance of the stripes of the debugging field background.
+ * @cssprop --wcp-preview-hint-debug-stripe-tilt - Tilt of the stripes of the debugging field background in degrees.
+ * @cssprop --wcp-preview-hint-debug-stripe-width - Width of the stripes of the debugging field background.
+ * @cssprop --wcp-preview-hint-debug-stripe-dash-size - Length of the dashes of the debugging field background.
+ * @cssprop --wcp-preview-hint-debug-stripe-dash-gap - Gap between the dashes of the debugging field background.
+ *
+ * @cssprop --wcp-preview-hint-debug-dark-background - Debugging field background color in dark mode.
+ * @cssprop --wcp-preview-hint-debug-dark-stroke - Debugging field dash and border color in dark mode.
+ *
+ * @cssprop --wcp-preview-hint-debug-light-background - Debugging field background color in light mode.
+ * @cssprop --wcp-preview-hint-debug-light-stroke - Debugging field dash and border color in light mode.
  */
 @customElement('wcp-preview-hint')
 export class PreviewHint extends ColorSchemable(LitElement) {
@@ -39,7 +59,7 @@ export class PreviewHint extends ColorSchemable(LitElement) {
     } else {
       const { height, width, x, y } = getRelativeBoundary(this.#element, this.#scrollParent);
       const { scrollTop = 0, scrollLeft = 0 } = this.#scrollParent ?? {};
-      
+
       this.style.setProperty('top', `${y + scrollTop}px`);
       this.style.setProperty('left', `${x + scrollLeft}px`);
       this.style.setProperty('height', `${height}px`);
