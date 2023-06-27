6db75c1c1e69e7353df90e165dd78fdba224c37d
David Enke <david@enke.dev>
2023-06-27 13:10:59 +0200

    feat(wcp-preview-frame-viewer-controls): use auto sizing slot editors

--- src/components/plugins/preview-frame-viewer/preview-frame-viewer-controls/preview-frame-viewer-controls.component.ts
+++ src/components/plugins/preview-frame-viewer/preview-frame-viewer-controls/preview-frame-viewer-controls.component.ts
@@ -15,11 +15,11 @@ import styles from './preview-frame-viewer-controls.component.scss';
 
 /**
  * @element wcp-preview-frame-viewer-controls
- * 
+ *
  * @cssprop --wcp-preview-frame-viewer-controls-headline-size - The font size of the headline.
  * @cssprop --wcp-preview-frame-viewer-controls-headline-weight - The font weight of the headline.
  * @cssprop --wcp-preview-frame-viewer-controls-headline-spacing - The inner spacing of the headline.
- * 
+ *
  * @cssprop --wcp-preview-frame-viewer-controls-dark-border-color - The border color of the element in dark mode.
  * @cssprop --wcp-preview-frame-viewer-controls-light-border-color - The border color of the element in light mode.
  *
@@ -108,6 +108,7 @@ export class PreviewFrameViewerControls extends ColorSchemable(LitElement) {
   protected renderSlotControl(slot: Parsed.Slot): TemplateResult {
     return html`
       <wcp-input-code
+        autosize
         name="slots.${slot.name}"
         label="${slot.name.trim() ? slot.name : 'Default'}"
         .value="${this.data?.slots[slot.name]}"
