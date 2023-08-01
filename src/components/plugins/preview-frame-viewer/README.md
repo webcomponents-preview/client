f0d8657d1a61c1fd1e781ef489a7fe49260ed940
David Enke <david@enke.dev>
2023-08-02 01:31:17 +0200

    feat(wcp-preview-frame-viewer): persist state in url

--- src/components/plugins/preview-frame-viewer/README.md
+++ src/components/plugins/preview-frame-viewer/README.md
@@ -4,9 +4,22 @@
 
 ## Properties
 
-| Property    | Attribute   | Modifiers | Type       | Default  |
-|-------------|-------------|-----------|------------|----------|
-| `available` | `available` | readonly  | `true`     | true     |
-| `element`   | `element`   |           |            |          |
-| `label`     | `label`     | readonly  | `"Viewer"` | "Viewer" |
-| `name`      | `name`      | readonly  | `"viewer"` | "viewer" |
+| Property    | Attribute   | Modifiers | Type                  | Default  |
+|-------------|-------------|-----------|-----------------------|----------|
+| `available` | `available` | readonly  | `true`                | true     |
+| `data`      | `data`      |           | `string \| undefined` |          |
+| `element`   | `element`   |           |                       |          |
+| `label`     | `label`     | readonly  | `"Viewer"`            | "Viewer" |
+| `name`      | `name`      | readonly  | `"viewer"`            | "viewer" |
+
+## Methods
+
+| Method                | Type                                |
+|-----------------------|-------------------------------------|
+| `#prepareElementData` | `(data?: string \| undefined): any` |
+
+## Events
+
+| Event                                  |
+|----------------------------------------|
+| `wcp-preview-frame-plugin:data-change` |
--- src/components/plugins/preview-frame-viewer/preview-frame-viewer.plugin.ts
+++ src/components/plugins/preview-frame-viewer/preview-frame-viewer.plugin.ts
@@ -1,16 +1,19 @@
 import { LitElement, type TemplateResult, html, unsafeCSS } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js';
+import { customElement, eventOptions, property, state } from 'lit/decorators.js';
 
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { keyed } from 'lit/directives/keyed.js';
 
 import { ColorSchemable } from '@/mixins/color-schemable.mixin.js';
+import { compress, decompress } from '@/utils/compression.utils.js';
 import type * as Parsed from '@/utils/parser.types.js';
 import type { PreviewFramePlugin } from '@/utils/plugin.utils.js';
 
 import { type ElementData, mapFormData, prepareInitialData } from './preview-frame-viewer.utils.js';
 
 import styles from './preview-frame-viewer.plugin.scss';
+
+const URI_DATA_PARAM_COMPRESSION: CompressionFormat = 'deflate-raw';
 
 /**
  * @element wcp-preview-frame-viewer
@@ -27,7 +30,11 @@ export class PreviewFrameViewer extends ColorSchemable(LitElement) implements Pr
   @property({ type: Object })
   set element(element: Parsed.Element | undefined) {
     this.#element = element;
-    this._elementData = element ? prepareInitialData(element) : undefined;
+  }
+
+  @property({ type: String })
+  set data(data: string | undefined) {
+    this.#prepareElementData(data);
   }
 
   @property({ type: Boolean, reflect: true })
@@ -39,15 +46,41 @@ export class PreviewFrameViewer extends ColorSchemable(LitElement) implements Pr
   @property({ type: String, reflect: true })
   readonly label = 'Viewer';
 
+  async #prepareElementData(data?: string) {
+    // read element data from compressed data param
+    if (data !== undefined) {
+      const elementData = await decompress(decodeURIComponent(data), URI_DATA_PARAM_COMPRESSION);
+      this._elementData = JSON.parse(elementData);
+    }
+    // otherwise we reset the data
+    else {
+      this._elementData = undefined;
+    }
+  }
+
   protected getElementReference(): Element | undefined {
     if (this.#element === undefined) return undefined;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return this.renderRoot.querySelector(this.#element.tagName!) ?? undefined;
   }
 
-  protected handleControlsInput(event: CustomEvent<FormData>) {
+  @eventOptions({ passive: true })
+  protected async handleControlsInput({ detail }: CustomEvent<FormData>) {
     if (this.#element === undefined) return;
-    this._elementData = mapFormData(event.detail, this.#element);
+    // prepare the data to be set as compressed url param
+    const data = mapFormData(detail, this.#element);
+    const param = encodeURIComponent(await compress(JSON.stringify(data), URI_DATA_PARAM_COMPRESSION));
+
+    // dispatch the event to update the url param
+    const event = new CustomEvent('wcp-preview-frame-plugin:data-change', { detail: param });
+    this.dispatchEvent(event);
+  }
+
+  protected override firstUpdated() {
+    // set initial state from element if no data is given
+    if (this.#element && this._elementData === undefined) {
+      this._elementData = prepareInitialData(this.#element);
+    }
   }
 
   protected override render(): TemplateResult {
