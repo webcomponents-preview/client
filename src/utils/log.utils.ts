76a8fbca61640e84a0ca1891bf3db269a476c248
David Enke <david@enke.dev>
2023-08-15 03:07:46 +0200

    feat: add log prefix

--- src/utils/log.utils.ts
+++ src/utils/log.utils.ts
@@ -1,19 +1,21 @@
 import { getConfig } from './config.utils.js';
 
+const LOG_PREFIX = '[wcp]';
+
 export const log = {
   info(...args: unknown[]) {
     if (['error', 'warn', 'info'].includes(getConfig()?.logging?.severity ?? 'info')) {
-      console.log(...args);
+      console.log(LOG_PREFIX, ...args);
     }
   },
   warn(...args: unknown[]) {
     if (['error', 'warn'].includes(getConfig()?.logging?.severity ?? 'info')) {
-      console.warn(...args);
+      console.warn(LOG_PREFIX, ...args);
     }
   },
   error(...args: unknown[]) {
     if (['error'].includes(getConfig()?.logging?.severity ?? 'info')) {
-      console.error(...args);
+      console.error(LOG_PREFIX, ...args);
     }
   },
 };
